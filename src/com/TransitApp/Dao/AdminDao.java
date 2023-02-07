da62770164c73a31b0f8411438fab0e78a091fab
Ouattara Katina <ouattarapevirgile@gmail.com>
2023-02-07 03:11:34 +0000

    controller pour se connecter en fonction des rôles

---
+++ src/com/TransitApp/Dao/AdminDao.java
@@ -0,0 +1,217 @@
+package com.TransitApp.Dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.TransitApp.Modeles.Admin;
+import com.TransitApp.Modeles.Fournisseur;
+import com.TransitApp.Util.HibernateUtil;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class AdminDao implements IAdminDao {
+
+	private Connection connect;
+
+	private PreparedStatement prepare;
+
+	private ResultSet result;
+
+	private double x;
+
+	private double y;
+
+    // save Student
+    // get All Students
+    // get Student By Id
+    // Update Student
+    // Delete Student
+
+    /* (non-Javadoc)
+     * @see net.javaguides.hibernate.dao.IStudentDao#saveStudent(net.javaguides.hibernate.model.Student)
+     */
+    @Override
+    public void saveAdmin(Admin admin) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start the transaction
+            transaction = session.beginTransaction();
+
+            // save student object
+            session.save(admin);
+
+            // commit the transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see net.javaguides.hibernate.dao.IStudentDao#updateStudent(net.javaguides.hibernate.model.Student)
+     */
+    @Override
+    public void updateAdmin(Admin admin) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start the transaction
+            transaction = session.beginTransaction();
+
+            // save student object
+            session.saveOrUpdate(admin);
+
+            // commit the transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see net.javaguides.hibernate.dao.IStudentDao#getStudentById(long)
+     */
+    @Override
+    public Admin getAdminById(int id) {
+        Transaction transaction = null;
+        Admin admin = null;
+        try {
+			Session session = HibernateUtil.getSessionFactory().openSession();
+			    // start the transaction
+			    transaction = session.beginTransaction();
+
+			    // get student object
+			    admin = session.byId(Admin.class).getReference(id);
+			     // or student = session.get(Student.class, id);
+			    //or student = session.load(Student.class, id);
+			   //or commit the transaction
+			    transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+                transaction.rollback();
+		}
+			
+		}
+        
+        
+        return admin;
+    }
+
+    /* (non-Javadoc)
+     * @see net.javaguides.hibernate.dao.IStudentDao#getAllStudents()
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public List < Admin > getAllAdmin() {
+        Transaction transaction = null;
+        List < Admin > admin = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start the transaction
+            transaction = session.beginTransaction();
+
+            // get admin
+            admin = session.createQuery("from Admin").list();
+           
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        return admin;
+    }
+
+    /* (non-Javadoc)
+     * @see net.javaguides.hibernate.dao.IStudentDao#deleteStudent(long)
+     */
+    @Override
+    public void deleteAdmin(int id) {
+        Transaction transaction = null;
+        Admin admin = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start the transaction
+            transaction = session.beginTransaction();
+
+            admin = session.get(Admin.class, id);
+            // get student object
+            session.delete(admin);
+            //student = session.load(Student.class, id);
+            // commit the transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    
+    public void Login(String fxml, Button button) {
+    	try {
+			Parent root;
+			root = FXMLLoader.load(getClass().getResource(fxml));
+
+			Stage stage = new Stage();
+
+			Scene scene = new Scene(root);
+
+			button.getScene().getWindow().hide();
+			
+			// Permet de faire bouger la fenetre et d'éviter de la redimensionner
+			root.setOnMousePressed((MouseEvent event) -> {
+				x = event.getSceneX();
+				y = event.getSceneY();
+			});
+
+			root.setOnMouseDragged((MouseEvent event) -> {
+				stage.setX(event.getScreenX() - x);
+				stage.setY(event.getScreenY() - y);
+			});
+
+			stage.setScene(scene);
+			stage.initStyle(StageStyle.TRANSPARENT);
+
+			stage.show();
+		} catch (IOException error) {
+			// TODO Auto-generated catch block
+			error.printStackTrace();
+		}
+    }
+	
+	/*
+	 * public List<Student > recupererCommandeByDateAndMenu(String idMenu , Date
+	 * dateCommande){ String query =
+	 * "SELECT `commande`.`DATECOMMANDE` FROM `commande` WHERE ((`commande`.`ID_MENU` = "
+	 * +idMenu+") AND (`commande`.`DATECOMMANDE` ='"+dateCommande+"'))"; //String
+	 * query = "SELECT * FROM `` WHERE ((`CODETYPE_LOGEMENT`='"
+	 * +typeLogement+"') AND (`CODE_TYPENATIONALITE` ='"
+	 * +typeNationalite+"') AND (`CODE_ANNEES`='"+codeAnne+"'))";
+	 * 
+	 * Commande commande = session.createSQLQuery().addEntity(getClass()).list();
+	 * (TypeLogementNationalite)
+	 * getSessionFactory().getCurrentSession().createSQLQuery(query).addEntity(
+	 * TypeLogementNationalite.class).uniqueResult(); return object; }
+	 */
+    
+    
+    
+}
---
+++ src/com/TransitApp/Dao/IAdminDao.java
@@ -0,0 +1,23 @@
+package com.TransitApp.Dao;
+
+import java.util.List;
+
+import com.TransitApp.Modeles.Admin;
+
+import javafx.scene.control.Button;
+
+public interface IAdminDao {
+
+	void saveAdmin(Admin admin);
+
+	void updateAdmin(Admin admin);
+
+	Admin getAdminById(int id);
+
+	List<Admin> getAllAdmin();
+
+	void deleteAdmin(int id);
+	
+	void Login(String fxml, Button button);
+
+}
