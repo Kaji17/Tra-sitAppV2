76065f1088667ab777830ad9cdbe68a14127a539
Ouattara Katina <ouattarapevirgile@gmail.com>
2023-02-07 22:38:27 +0000

    bugs

--- src/com/TransitApp/Controllers/ManagerController.java
+++ src/com/TransitApp/Controllers/ManagerController.java
@@ -5,13 +5,13 @@ import java.util.Optional;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 public class ManagerController {
